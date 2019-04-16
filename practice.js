48daf6f13d0926a1c4aa7573e6889e48956f7224
scottschardt <40869802+scottschardt@users.noreply.github.com>
2019-04-16 09:43:18 -0700

    update practice.js
    Using TDD to solve the roman numeral problem.

--- practice.js
+++ practice.js
@@ -1,133 +1,57 @@
 
-class RomanNumeral {
-    constructor(){
-      this.four = 'IV',
-      this.nine = 'IX',
-      this.forty = 'XL',
-      this.ninety = 'XC',
-      this.fourhundred = 'CD',
-      this.ninehundred = 'CM'
-    }
-    
-    getOddNumbers(int){
-        let romanNumeralString = "";
-        romanNumeralString = int == 4 ? this.four: romanNumeralString
-        romanNumeralString = int == 9 ? this.nine: romanNumeralString
-        romanNumeralString = int == 40 ? this.forty: romanNumeralString
-        romanNumeralString = int == 90 ? this.ninety: romanNumeralString
-        romanNumeralString = int == 400 ? this.fourhundred: romanNumeralString
-        romanNumeralString = int == 900 ? this.ninehundred: romanNumeralString
-        return romanNumeralString
-    }
-}
-let x = 39809;
-console.log(x.toString().length)
-
-function getRomanNumeral(int) {
-    let romanNumeralArray = ["M", "D", "C", "L", "X", "V", "I"];
-    let romanNumeralValueArray = [1000, 500, 100, 50, 10, 5, 1];
-    let result = "";
-    let original = int;
-    let romanObject = new RomanNumeral();
-    if(int == '' || int == null || int == undefined || typeof int === "string" || int == 0) {
-        return result = "Not able to get Roman Numeral"
+function assertEquals(actual, expected){
+    if(actual == expected){
+        console.log(`Test Passed! Actual: ${actual} - Expected: ${expected}`)
     } else {
-        for(let i = 0; i < romanNumeralArray.length;i++){
-            if(original/romanNumeralValueArray[i] >= 1){
-                let quotient = (original/romanNumeralValueArray[i]);
-                let integer = Math.floor(quotient);
-                if(quotient >= 1.8 && quotient < 2 || integer == 4 && original < 9000) {
-                    let originalString = original.toString();
-                    let originalStringLength = originalString.length;
-                    let firstInt = originalString.substring(0, 1);
-                    let factor = parseInt(firstInt, 10);
-                    let number = factor * (10**(originalStringLength-1));
-                    
-                    let oddNumber = romanObject.getOddNumbers(number);
-                    result += oddNumber;
-                    original -= number;
-                } else {
-                        original -= (integer*romanNumeralValueArray[i]);
-                        result += romanNumeralArray[i].repeat(integer)
-                }
-            }
-        }
+        console.log(`Test Failed! Actual: ${actual} - Expected: ${expected}`)
     }
-    return result;
+}
+
+var RomanNumeral = function() {
+}
+
+var getNumeralObject = function(int, romanNumeral) {
+    return {value: int, numeral: romanNumeral}
+}
+
+var numeralArray = function(){
+    let numeralArray = [];
+    numeralArray.push(getNumeralObject(1000, "M"));
+    numeralArray.push(getNumeralObject(900, "CM"));
+    numeralArray.push(getNumeralObject(500, "D"));
+    numeralArray.push(getNumeralObject(400, "CD"));
+    numeralArray.push(getNumeralObject(100, "C"));
+    numeralArray.push(getNumeralObject(90, "XC"));
+    numeralArray.push(getNumeralObject(50, "L"));
+    numeralArray.push(getNumeralObject(40, "XL"));
+    numeralArray.push(getNumeralObject(10, "X"));
+    numeralArray.push(getNumeralObject(9, "IX"));
+    numeralArray.push(getNumeralObject(5, "V"));
+    numeralArray.push(getNumeralObject(4, "IV"));
+    numeralArray.push(getNumeralObject(1, "I"));
+    return numeralArray;
+}
+
+RomanNumeral.prototype.translator = function (int) {
+    let romanNumeralString = "";
+    numeralArray().forEach((item) => {
+        for(; int >= item.value; int -= item.value){
+            romanNumeralString += item.numeral;
+        }
+    })
+    return romanNumeralString
 }
 
 
 
-let test = (component, fn, count =1) => {
-    console.log(`#${component} Test` );
-    let unitTest = (actual, expected, msg) => {
-        if(actual === expected){
-            console.log(`#${count} test passed! ${msg}. Actual: ${actual} - Expected: ${expected}`)
-        } else {
-            console.log(`#${count} test failed! ${msg}. Actual: ${actual} - Expected: ${expected}`)
-        }
-        count++
-    }
-    fn(unitTest);
-}
+let getRomanNumeral = new RomanNumeral();
 
-test('RomanNumeral Translator', assert => {
-    {
-        const msg = 'Get a Roman Numerals 1 through 3';
-        const actual = getRomanNumeral(3);
-        const expected = "III";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'return a roman numeral for all numbers excluding those with a 4 or 9';
-        const actual = getRomanNumeral(853);
-        const expected = "DCCCLIII";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'return a roman numeral for all numbers';
-        const actual = getRomanNumeral(900);
-        const expected = "CM";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing odd numbers';
-        const actual = getRomanNumeral(9);
-        const expected = "IX";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing odd numbers';
-        const actual = getRomanNumeral(4);
-        const expected = "IV";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing odd numbers';
-        const actual = getRomanNumeral(999);
-        const expected = "CMXCIX";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing edge cases';
-        const actual = getRomanNumeral("a;skdfj asd");
-        const expected = "Not able to get Roman Numeral";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing edge cases';
-        const actual = getRomanNumeral(0);
-        const expected = "Not able to get Roman Numeral";
-        assert(actual, expected, msg);
-    }
-    {
-        const msg = 'testing edge cases';
-        const actual = getRomanNumeral(0);
-        const expected = "Not able to get Roman Numeral";
-        assert(actual, expected, msg);
-    }
-
-    
-})
+assertEquals(getRomanNumeral.translator(1), "I")
+assertEquals(getRomanNumeral.translator(2), "II")
+assertEquals(getRomanNumeral.translator(3), "III")
+assertEquals(getRomanNumeral.translator(4), "IV")
+assertEquals(getRomanNumeral.translator(9), "IX")
+assertEquals(getRomanNumeral.translator(20), "XX")
+assertEquals(getRomanNumeral.translator(1956), "MCMLVI")
 
 
